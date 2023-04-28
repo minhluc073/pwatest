0c25997d933e65b51d325ad34810e6374d0c242d
minhluc073 <nguyenminhluc0703@gmail.com>
2023-04-28 12:01:13 +0700

    update

--- javascript/countto.js
+++ javascript/countto.js
@@ -1,20 +1,85 @@
 (function ($) {
-  $(".count-number").each(function () {
-    var size = $(this).text().split(".")[1]
-      ? $(this).text().split(".")[1].length
-      : 0;
-    $(this)
-      .prop("Counter", 0)
-      .animate(
+  $.fn.countTo = function (options) {
+    options = options || {};
+
+    return $(this).each(function () {
+      // set options for current element
+      var settings = $.extend(
+        {},
+        $.fn.countTo.defaults,
         {
-          Counter: $(this).text(),
+          from: $(this).data("from"),
+          to: $(this).data("to"),
+          speed: $(this).data("speed"),
+          refreshInterval: $(this).data("refresh-interval"),
+          decimals: $(this).data("decimals"),
         },
-        {
-          duration: 5000,
-          step: function (func) {
-            $(this).text(parseFloat(func).toFixed(size));
-          },
-        }
+        options
       );
-  });
+
+      // how many times to update the value, and how much to increment the value on each update
+      var loops = Math.ceil(settings.speed / settings.refreshInterval),
+        increment = (settings.to - settings.from) / loops;
+
+      // references & variables that will change with each update
+      var self = this,
+        $self = $(this),
+        loopCount = 0,
+        value = settings.from,
+        data = $self.data("countTo") || {};
+
+      $self.data("countTo", data);
+
+      // if an existing interval can be found, clear it first
+      if (data.interval) {
+        clearInterval(data.interval);
+      }
+      data.interval = setInterval(updateTimer, settings.refreshInterval);
+
+      // initialize the element with the starting value
+      render(value);
+
+      function updateTimer() {
+        value += increment;
+        loopCount++;
+
+        render(value);
+
+        if (typeof settings.onUpdate == "function") {
+          settings.onUpdate.call(self, value);
+        }
+
+        if (loopCount >= loops) {
+          // remove the interval
+          $self.removeData("countTo");
+          clearInterval(data.interval);
+          value = settings.to;
+
+          if (typeof settings.onComplete == "function") {
+            settings.onComplete.call(self, value);
+          }
+        }
+      }
+
+      function render(value) {
+        var formattedValue = settings.formatter.call(self, value, settings);
+        $self.text(formattedValue);
+      }
+    });
+  };
+
+  $.fn.countTo.defaults = {
+    from: 0, // the number the element should start at
+    to: 0, // the number the element should end at
+    speed: 1000, // how long it should take to count between the target numbers
+    refreshInterval: 100, // how often the element should be updated
+    decimals: 0, // the number of decimal places to show
+    formatter: formatter, // handler for formatting the value before rendering
+    onUpdate: null, // callback method for every time the element is updated
+    onComplete: null, // callback method for when the element finishes updating
+  };
+
+  function formatter(value, settings) {
+    return value.toFixed(settings.decimals);
+  }
 })(jQuery);
--- javascript/main.js
+++ javascript/main.js
@@ -215,32 +215,46 @@
       var a = 0;
       $(window).scroll(function () {
         var oTop = $(".wrap-counter").offset().top - window.innerHeight;
-        if ($(window).scrollTop() > oTop) {
+        if (a == 0 && $(window).scrollTop() > oTop) {
           if ($().countTo) {
-            $(".stat-number").each(function () {
-              var size = $(this).text().split(".")[1]
-                ? $(this).text().split(".")[1].length
-                : 0;
-              $(this)
-                .prop("Counter", 0)
-                .animate(
-                  {
-                    Counter: $(this).text(),
-                  },
-                  {
-                    duration: 3000,
-                    step: function (func) {
-                      $(this).text(parseFloat(func).toFixed(size));
-                    },
-                  }
-                );
-            });
+            $(".wrap-counter")
+              .find(".counter-number")
+              .each(function () {
+                var to = $(this).data("to"),
+                  speed = $(this).data("speed"),
+                  formatter = $(this).data("formatter");
+                $(this).countTo({
+                  to: to,
+                  speed: speed,
+                  formatter: formatter,
+                });
+              });
           }
           a = 1;
         }
       });
     }
   };
+
+  $(".number").each(function () {
+    var size = $(this).text().split(".")[1]
+      ? $(this).text().split(".")[1].length
+      : 0;
+    $(this)
+      .prop("Counter", 0)
+      .animate(
+        {
+          Counter: $(this).text(),
+        },
+        {
+          duration: 5000,
+          step: function (func) {
+            $(this).text(parseFloat(func).toFixed(size));
+          },
+        }
+      );
+  });
+
   /* filter
   ------------------------------------------------------------------------------------- */
   var filter = function () {
@@ -486,20 +500,7 @@
   };
   /* btn nav menu
   ------------------------------------------------------------------------------------- */
-  var btnmenu = function () {
-    $(document).on("click", ".menu-item-has-children-mobile", function () {
-      var args = { duration: 600 };
-      if ($(this).hasClass("active")) {
-        $(this).children(".sub-menu-mobile").slideUp(args);
-        $(this).removeClass("active");
-      } else {
-        $(".sub-menu-mobile").slideUp(args);
-        $(this).children(".sub-menu-mobile").slideDown(args);
-        $(".menu-item-has-children-mobile").removeClass("active");
-        $(this).addClass("active");
-      }
-    });
-  };
+
   /* btnCategory
   ------------------------------------------------------------------------------------- */
   var btnCategory = function () {
@@ -518,54 +519,14 @@
   };
   /* dropOptionForm 
   ------------------------------------------------------------------------------------- */
-  var dropOptionForm = function () {
-    if ($("select").length > 0) {
-      $(
-        "select:not(#billing_country):not(.country_select):not(#billing_state)"
-      ).niceSelect();
-    }
-  };
 
-  new WOW().init();
+  // new WOW().init();
 
-  $(document).ready(function () {
-    var height = $(window).height() + 20;
-    $(".row-height").css("height", height);
-    $(".row-height").css("max-height", height);
-  });
-
-  var setTimeIn = function () {
-    if ($(".wd-popup-form").length > 0) {
-      setTimeout(function () {
-        $(".wd-popup-form").addClass("modal-menu--open");
-      }, 3000);
-    }
-  };
   /* Modal_Right
   ------------------------------------------------------------------------------------- */
   var Modal_Right = function () {
     const body = $("body");
-    const modalMenu = $(".sidebar-popup");
-    const modalNav = $(".menu-mobile-popup");
-    const modalMenu2 = $(".wd-popup-form");
-    const modalMenu3 = $(".wd-popup-job-apply");
-    const modalMenu4 = $(".wd-filter-radio");
-
-    if (modalMenu.length) {
-      const open = function () {
-        modalMenu.addClass("modal-menu--open");
-      };
-      const close = function () {
-        modalMenu.removeClass("modal-menu--open");
-      };
-
-      $(".button-filter").on("click", function () {
-        open();
-      });
-      $(".modal-menu__backdrop, .title-button-group").on("click", function () {
-        close();
-      });
-    }
+    const modalNav = $(".menu-popup");
     if (modalNav.length) {
       const open = function () {
         modalNav.addClass("modal-menu--open");
@@ -574,46 +535,11 @@
         modalNav.removeClass("modal-menu--open");
       };
 
-      $(".nav-filter").on("click", function () {
+      $("#nav-filter").on("click", function () {
         open();
       });
-      $(".modal-menu__backdrop, .title-button-group").on("click", function () {
+      $(".modal-menu__backdrop, .menu-content li").on("click", function () {
         close();
-      });
-    }
-    if (modalMenu2.length) {
-      const close = function () {
-        modalMenu2.addClass("modal-menu--close");
-        modalMenu2.removeClass("modal-menu--open");
-      };
-      $(".modal-menu__backdrop, .title-button-group").on("click", function () {
-        close();
-      });
-    }
-    if (modalMenu3.length) {
-      const open = function () {
-        modalMenu3.addClass("modal-menu--open");
-      };
-      const close = function () {
-        modalMenu3.removeClass("modal-menu--open");
-      };
-      $(".btn-popup").on("click", function () {
-        open();
-      });
-      $(".modal-menu__backdrop").on("click", function () {
-        close();
-      });
-    }
-    if (modalMenu4.length) {
-      $(".filter-radio").on("click", function () {
-        modalMenu4.toggleClass("modal-menu--open");
-      });
-      $(document).on("click.filter-radio", function (a) {
-        if (
-          $(a.target).closest(".filter-radio, .wd-filter-radio").length === 0
-        ) {
-          modalMenu4.removeClass("modal-menu--open");
-        }
       });
     }
   };
@@ -692,30 +618,11 @@
   /* Dom Ready */
   $(function () {
     headerFixed();
-    showsearch();
-    dropOptionForm();
-    flatProgressBar();
-    accordion();
     goTop();
-    counter();
-    setTimeIn();
-    tfTabs();
-    filter();
-    stickSidebar();
-    parallax();
-    gallery();
     scrollEffect();
-    btnQuantity();
-    loadmore();
-    loadmore2();
-    UpImg();
-    delete_img();
     Modal_Right();
+    counter();
     popUpLightBox();
-    progressProduct();
-    rangeSlider();
-    btnmenu();
-    btnCategory();
     preload();
   });
 })(jQuery);
